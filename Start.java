3a577e14290ec3ab46220c00b8ce7f008eb105b0
David Sarkies <david.sarkies@internode.on.net>
2024-09-07 21:06:48 +1000

    Created repository and initial files

---
+++ Start.java
@@ -0,0 +1,18 @@
+/*
+Title: Island of Secrets Starter
+Author: Jenny Tyler & Les Howarth
+Translator: David Sarkies
+Version: 0.0
+Date: 7 September 2024
+Source: https://archive.org/details/island-of-secrets_202303
+*/
+
+public class Start {
+	
+	public static void main(String[] args) {
+		
+		Main game = new Main();
+		//game.startGame();
+
+	}
+}
