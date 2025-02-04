dd556acec4b00090ec6ec496c58e1cd913f6b310
David Sarkies <david.sarkies@internode.on.net>
2025-02-04 21:20:45 +1100

    Added border for the map panel

--- View/MapPanel.java
+++ View/MapPanel.java
@@ -2,15 +2,19 @@
 Title: Island of Secrets Map Panel
 Author: Jenny Tyler & Les Howarth
 Translator: David Sarkies
-Version: 3.0
-Date: 2 February 2025
+Version: 3.1
+Date: 4 February 2025
 Source: https://archive.org/details/island-of-secrets_202303
 */
 
 package View;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Model.GameEngine;
@@ -26,10 +30,38 @@ public class MapPanel extends JPanel {
 		this.engine = engine;
 		
 		setLayout(new GridLayout(11,10));
+		
+		for (int x=1;x<110;x++) {
+			
+			JPanel roomPanel = new JPanel();
+			
+			roomPanel.add(new JLabel(""+x));
+			
+			if (x<10 && x>1) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(2, 0, 0, 0, Color.BLACK));
+			} else if (x==1) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(2, 2, 0, 0, Color.BLACK));
+			} else if (x==10) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(2, 0, 0, 2, Color.BLACK));
+			} else if (x==71) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(0, 2, 2, 0, Color.BLACK));
+			} else if (x==80) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 2, Color.BLACK));
+			} else if (x>71 && x<80) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
+			} else if (x==11 || x==21 || x==31 || x==41 || x==51 || x==61) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(0, 2, 0, 0, Color.BLACK));
+			} else if (x==20 || x==30 || x==40 || x==50 || x==60 || x==70) {
+				roomPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.BLACK));
+			}
+			
+			add(roomPanel);
+		}
 	}
 
 }
 
 /*
  * 2 February 2025 - Created File
+ * 4 February 2025 - Added Borders to the map panel.
  */
