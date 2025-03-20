7e9ab2bd51beb8cba5337e52ba10a2afe6f2c1bf
David Sarkies <david.sarkies@internode.on.net>
2025-03-20 13:29:21 +1100

    Completed the message builder

--- Model/MessageBuilder.java
+++ Model/MessageBuilder.java
@@ -2,8 +2,8 @@
 Title: Island of Secrets Message Builder Class
 Author: Jenny Tyler & Les Howarth
 Translator: David Sarkies
-Version: 4.0
-Date: 19 March 2025
+Version: 4.1
+Date: 20 March 2025
 Source: https://archive.org/details/island-of-secrets_202303
 */
 
@@ -67,15 +67,64 @@ public class MessageBuilder {
 		}
 	}
 	
-	public void concatMessage(String message) {
-		this.message.add(message);
-	}
+	/**
+     * Adds a message that should be concatenated with the last message if possible.
+     * If the last message ends with a period, the new message starts on a new line.
+     * Otherwise, it is appended with a comma.
+     */
+    public void addLongMessage(String message) {
+    	
+    	if (message != null && !message.isEmpty()) {
+    		if (messages.isEmpty()) {
+    			//First message, add it directly
+    			messages.add(message);
+    		} else {
+    			
+    			//Get last message
+    			String lastMessage = messages.get(messages.size()-1);
+    			
+    			//Determine how to concatenate
+    			if (lastMessage.endsWith(".")) {
+    				
+    				//Start new sentance
+    				lastMessage += " " + message; 
+    			} else {
+    				
+    				//Append the current sentance
+    				lastMessage += ", " + message;
+    			}
+    			
+    			//Check if the concatenated message exceeds the maximum length
+    			if (lastMessage.length() <= maxMessageLength) {
+    				
+    				//Update the last message
+    				messages.set(messages.size()-1, lastMessage);
+    			} else {
+    				
+    				//Split the concatenated message
+    				messages.remove(messages.size()-1);
+    				addMessage(lastMessage);
+    			}
+    		}
+    	}
+    	
+    }
 	
-	public List<String> getMessage() {
-		return this.message;
-	}
+    /**
+     * Returns the list of formatted messages.
+     */
+    public List<String> getMessages() {
+        return new ArrayList<>(messages); // Return a copy to prevent external modification
+    }
+
+    /**
+     * Clears all messages from the builder.
+     */
+    public void clearMessages() {
+        messages.clear();
+    }
 }
 
 /* 19 March 2025 - Created Class
- * 
+ * 20 March 2025 - Completed class based on recommendations
  */
