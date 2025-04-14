5202c6b4864a692b00bd941b51e057d3a606bd7e
David Sarkies <david.sarkies@internode.on.net>
2025-04-14 13:08:46 +1000

    Updated Game State provider

--- Model/GameState.java
+++ Model/GameState.java
@@ -2,61 +2,91 @@
 Title: Island of Secrets Game State
 Author: Jenny Tyler & Les Howarth
 Translator: David Sarkies
-Version: 4.3
-Date: 4 April 2025
+Version: 4.4
+Date: 14 April 2025
 Source: https://archive.org/details/island-of-secrets_202303
 */
 
 package Model;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import Interfaces.GameStateProvider;
 
+/**
+ * Immutable snapshot of game state implementing GameStateProvider interface.
+ * Thread-safe by design - all fields are final and collections are unmodifiable.
+ */
 public class GameState implements GameStateProvider {
 
+	//Core game state
 	private final String room;
 	private final String items;
 	private final String exits;
+	private final String specialExits;
 	private final String time;
 	private final String status;
-	private final String specialExits;
-	private final String[] displayedSavedGames;
-	private final String[] previousCommands;
-	private final List<String> message;
-	private final int finalScore;
+
+	//UI State
 	private final int responseType;
 	private final int panelFlag;
+	
+	//Game progression
 	private final int currentRoom;
-	private final boolean lowerLimit;
-	private final boolean upperLimit;
+	private final int finalScore;
 	private final boolean initialGameState;
 	private final boolean saveGameState;
 	private final boolean endGameState;
-	private final GameStateProvider state;
 	
-	public GameState(GameStateProvider state) {
-		this.room = state.getRoom();
-		this.items = state.getItems();
-		this.exits = state.getExits();
-		this.time = state.getTime();
-		this.status = state.getStatus();
-		this.specialExits = state.getSpecialExits(); 
-		this.displayedSavedGames = state.getDisplayedSavedGames();
-		this.previousCommands = state.getCommands();
-		this.message = state.getMessage();
-		this.finalScore = state.getFinalScore();
-		this.initialGameState = state.isInitialGameState();
-		this.saveGameState = state.isSavedGameState();
-		this.endGameState = state.isEndGameState();
-		this.lowerLimit = state.getLowerLimitSavedGames();
-		this.upperLimit = state.getUpperLimitSavedGames();
-		this.responseType = state.getResponseType();
-		this.panelFlag = state.getPanelFlag();
-		this.currentRoom = state.getCurrentRoom();
-		this.state = state;
+	//Saved games navigation
+	private final String[] displayedSavedGames;
+	private final boolean lowerLimit;
+	private final boolean upperLimit;
+	
+	//Command history
+	private final String[] previousCommands;
+	private final List<String> message;
+	
+	//Delegate for dynamic data
+	private final GameStateProvider stateProvider;
+	
+	public GameState(GameStateProvider stateProvider) {
+		Objects.requireNonNull(stateProvider,"State provider cannot be null");
+		
+		this.stateProvider = stateProvider;
+		
+		//Core state
+		this.room = stateProvider.getRoom();
+		this.items = stateProvider.getItems();
+		this.exits = stateProvider.getExits();
+		this.specialExits = stateProvider.getSpecialExits();
+		this.time = stateProvider.getTime();
+		this.status = stateProvider.getStatus();
+
+		//UI state
+		this.responseType = stateProvider.getResponseType();
+		this.panelFlag = stateProvider.getPanelFlag();
+		
+		//Game progression
+		this.currentRoom = stateProvider.getCurrentRoom();
+		this.finalScore = stateProvider.getFinalScore();
+		this.initialGameState = stateProvider.isInitialGameState();
+		this.saveGameState = stateProvider.isSavedGameState();
+		this.endGameState = stateProvider.isEndGameState();
+		
+		//Saved games
+		this.displayedSavedGames = stateProvider.getDisplayedSavedGames().clone();
+		this.lowerLimit = stateProvider.getLowerLimitSavedGames();
+		this.upperLimit = stateProvider.getUpperLimitSavedGames();
+		
+		//Command history
+		this.previousCommands = stateProvider.getCommands().clone();
+		this.message = Collections.unmodifiableList(stateProvider.getMessage());
 	}
 	
+	//Implemented interface methods
 	@Override
 	public String getRoom() {
 		return this.room;
@@ -86,27 +116,27 @@ public class GameState implements GameStateProvider {
 	public String getStatus() {
 		return this.status;
 	}
-
+	
 	@Override
-	public String[] getDisplayedSavedGames() {
-		return this.displayedSavedGames;
+	public int getPanelFlag() {
+		return this.panelFlag;
 	}
 
 	@Override
-	public String[] getCommands() {
-		return this.previousCommands;
+	public int getResponseType() {
+		return this.responseType;
 	}
-
+	
 	@Override
-	public List<String> getMessage() {
-		return this.message;
+	public int getCurrentRoom() {
+		return this.currentRoom;
 	}
 	
 	@Override
 	public int getFinalScore() {
 		return this.finalScore;
 	}
-
+	
 	@Override
 	public boolean isInitialGameState() {
 		return this.initialGameState;
@@ -121,6 +151,11 @@ public class GameState implements GameStateProvider {
 	public boolean isEndGameState() {
 		return this.endGameState;
 	}
+	
+	@Override
+	public String[] getDisplayedSavedGames() {
+		return this.displayedSavedGames.clone();
+	}
 
 	@Override
 	public boolean getLowerLimitSavedGames() {
@@ -131,41 +166,37 @@ public class GameState implements GameStateProvider {
 	public boolean getUpperLimitSavedGames() {
 		return this.upperLimit;
 	}
-
+	
 	@Override
-	public int getResponseType() {
-		return this.responseType;
+	public String[] getCommands() {
+		return this.previousCommands.clone();
 	}
 
 	@Override
-	public int getPanelFlag() {
-		return this.panelFlag;
+	public List<String> getMessage() {
+		return this.message;
 	}
-
+	
+	//Dynamic data delegation
 	@Override
 	public boolean getRoomVisited(int roomNumber) {
-		return state.getRoomVisited(roomNumber);
+		return stateProvider.getRoomVisited(roomNumber);
 	}
 
 	@Override
 	public boolean[] getRoomExits(int roomNumber) {
-		return state.getRoomExits(roomNumber);
+		return stateProvider.getRoomExits(roomNumber);
 	}
 
 	@Override
 	public String getRoomImageType(int roomNumber) {
-		return state.getRoomImageType(roomNumber);
+		return stateProvider.getRoomImageType(roomNumber);
 	}
-
-	@Override
-	public int getCurrentRoom() {
-		return this.currentRoom;
-	}
-
 }
 
 /* 1 April 2025 - Created File
  * 2 April 2025 - Added up to endGameState
  * 3 April 2025 - Completed Game State
  * 4 April 2025 - Updated for new functions added to interface
+ * 14 April 2025 - Updated based on Deepseek recommendations
  */
