d15e8a1dc02241f2bc66f26e6d14b7c7957eeef4
David Sarkies <david.sarkies@internode.on.net>
2025-05-22 19:58:13 +1000

    Made Command Normaliser a private class

--- Commands/CommandParser.java
+++ Commands/CommandParser.java
@@ -2,8 +2,8 @@
 Title: Island of Secrets Command Parser
 Author: Jenny Tyler & Les Howarth
 Translator: David Sarkies
-Version: 4.3
-Date: 2 May 2025
+Version: 4.7
+Date: 22 May 2025
 Source: https://archive.org/details/island-of-secrets_202303
 */
 
@@ -116,6 +116,35 @@ public class CommandParser {
 	private ParsedCommand parseMove(ParsedCommand command,int room) {
 		return new Move().parseMove(command, room);
 	}
+	
+	private class CommandNormaliser {
+
+		public String normalise(String input) {
+
+			input = input.toLowerCase();
+			
+			if (input.equals("u") || input.equals("up")) {
+				input = "go up";
+			} else if (input.equals("d") || input.equals("down")) {
+				input = "go down";
+			} else if (input.equals("i") || input.equals("enter") ||
+					input.equals("inside") || input.equals("go inside")) {
+				input = "go in";
+			} else if (input.equals("o") || input.equals("exit") ||				
+					input.equals("outside") || input.equals("go outside")) {
+				input = "go out";
+			} else if (input.equals("north")) {
+				input = "n";
+			} else if (input.equals("south")) {
+				input = "s";
+			} else if (input.equals("east")) {
+				input = "e";
+			} else if (input.equals("west")) {
+				input = "w";
+			}			
+			return input;
+		}
+	}
 }
 
 /* 28 April 2025 - Created File
@@ -124,4 +153,5 @@ public class CommandParser {
  * 2 May 2025 - Updated for command validator
  * 3 May 2025 - Added Parse Look method
  * 4 May 2025 - Added Parse Move method
+ * 22 May 2025 - Moved CommandNormaliser here as private class
  */
