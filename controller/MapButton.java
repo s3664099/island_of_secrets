373ba5495a75452a0c75305558e68747d41f0abb
David Sarkies <david.sarkies@internode.on.net>
2025-09-18 21:02:02 +1000

    Added JavaDocs

--- controller/MapButton.java
+++ controller/MapButton.java
@@ -15,14 +15,41 @@ import java.util.Objects;
 
 import view.GamePanel;
 
+
+/**
+ * An {@link ActionListener} that switches the {@link GamePanel}
+ * to its map view when the associated UI control (such as a button)
+ * is activated.
+ * <p>
+ * Typical usage:
+ * <pre>{@code
+ * JButton mapButton = new JButton("Map");
+ * mapButton.addActionListener(new MapButton(gamePanel));
+ * }</pre>
+ */
 public class MapButton implements ActionListener {
 
+    /** The game panel whose map view will be displayed. */
 	private final GamePanel panel;
 	
+    /**
+     * Constructs a {@code MapButton} that will show the map view
+     * on the specified {@link GamePanel}.
+     *
+     * @param panel the game panel that provides the map view;
+     *              must not be {@code null}
+     * @throws NullPointerException if {@code panel} is {@code null}
+     */
 	public MapButton(GamePanel panel) {
 			this.panel = Objects.requireNonNull(panel,"GamePanel cannot be null");
 	}
 	
+    /**
+     * Invoked when an action occurs on the associated UI component.
+     * This implementation switches the {@link GamePanel} to its map view.
+     *
+     * @param event the action event triggered by the user interaction
+     */
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		panel.showMapView();
